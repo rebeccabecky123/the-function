990899a6c22c50ae404f0780e3742e92318a82e0
Byamungu Rebecca <rebeccabyamungu6@gmail.com>
2025-04-10 05:48:10 -0700

    Add files via upload

---
+++ string.js
@@ -0,0 +1,18 @@
+
+// is thing build by js of your self to know and to use it 
+// build-trim
+
+function trim(name){
+return name.trim()
+}
+function toLowerCase(name){
+    return name.toLowerCase()
+}
+function compose(name){
+    return "@" + name
+}
+let username = compose(toLowerCase(trim(" DOSTA ")))
+console.log(username)
+
+// there are more function to make one funtion (iyo imwe iba ikubiyemo byose nukuvuga all function )
+// urabiteranyiriza hamwe bikavamo ikintu kimwe 
