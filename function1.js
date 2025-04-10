54d85e033f1eb9b94c50229c53e7df9de6ca13ab
Byamungu Rebecca <rebeccabyamungu6@gmail.com>
2025-04-10 05:47:18 -0700

    Add files via upload

---
+++ function1.js
@@ -0,0 +1,52 @@
+
+
+// 1. Function Declaration
+
+function sayHello(){
+
+    console.log("Hello!");
+    
+}
+    sayHello();
+
+// exmple(2).
+      function greet(){
+
+        console.log("hello there how  are you i miss you !");
+}
+          greet();
+
+// example in real life
+
+           function cleaninghouse(){
+    console.log("take omo a soap and cleaning water");
+    console.log("take Basket");
+    console.log("take rackcret");
+    console.log("the house isclean now");
+}
+             cleaninghouse();  
+// twahamagaye izo function zose
+
+// another example when there is something we want to increase in function 
+
+               function greetperson(name){
+
+                 console.log("hello how are you " + name + "!");
+}
+                   greetperson("keza");
+
+                      greetperson("kelly");
+
+                        greetperson("becky");
+
+
+//  another examples
+//  Function Declaration with Return (igarura agaciro):
+
+                          function add (a,b){
+
+                           return a +b;
+}
+                            let result = add (3, 4);
+
+                              console.log(result);
